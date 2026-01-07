047c02ba7bd42ef521e613cdf0abf5af6de43d79
katoy <youichikato@gmail.com>
2026-01-07 10:11:21 +0900

    すべての操作を使ったスクランブルのテストを追加
    - test_all_moves_orientation_alignment を追加
    - R, L, F, B, U, Dのすべての操作を使ったスクランブルをテスト
    - 向きも揃える解法が正しく動作することを確認
    
    テスト結果: 成功 (24.39秒)

--- 2x2/tests/test_orientation_verification.rs
+++ 2x2/tests/test_orientation_verification.rs
@@ -62,3 +62,39 @@ fn test_complex_orientation_alignment() {
         solution.moves
     );
 }
+
+/// すべての操作(R, L, F, B, U, D)を使ったスクランブルのテスト
+#[test]
+fn test_all_moves_orientation_alignment() {
+    let mut cube = Cube::new();
+
+    // すべての操作を使ったスクランブル
+    cube.apply_move(Move::R);
+    cube.apply_move(Move::L);
+    cube.apply_move(Move::F);
+    cube.apply_move(Move::B);
+    cube.apply_move(Move::U);
+    cube.apply_move(Move::D);
+
+    // 向きも揃える解法を探索
+    let solution = solver::solve(&cube, 14, false);
+
+    assert!(solution.found, "解が見つかるべき");
+
+    // 解法を適用
+    let mut test_cube = cube.clone();
+    for &mv in &solution.moves {
+        test_cube.apply_move(mv);
+    }
+
+    // 完全に解けているか確認
+    assert!(
+        solver::is_fully_solved(&test_cube),
+        "解法適用後、完全に解けているべき。解法: {:?}",
+        solution.moves
+    );
+
+    // 初期状態と一致するか確認
+    let initial_cube = Cube::new();
+    assert_eq!(test_cube, initial_cube, "解法適用後、初期状態と一致すべき");
+}
